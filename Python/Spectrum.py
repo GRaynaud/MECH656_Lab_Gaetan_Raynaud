4529f3557d9e9b122423ea853960771e2ff239c5
GRaynaud <gaetan.raynaud@polytechnique.edu>
2020-04-14 09:37:03 -0400

    adjustements

--- Python/Spectrum.py
+++ Python/Spectrum.py
@@ -19,12 +19,22 @@ import tikzplotlib
 # Acquisition des données
 # =============================================================================
 
-filename = 'TraitementDataSpec/specrsltfilnam020.11mar2020.txt'
+filename0020 = 'TraitementDataSpec/specrsltfilnam020.11mar2020.txt'
+filename0550 = 'TraitementDataSpec/specrsltfilnam550.11mar2020.txt'
+filename2000 = 'TraitementDataSpec/specrsltfilnam2000.11mar2020.txt'
 
-T = np.genfromtxt(filename,skip_header=1)
+T0020 = np.genfromtxt(filename0020,skip_header=1)
+T0550 = np.genfromtxt(filename0550,skip_header=1)
+T2000 = np.genfromtxt(filename2000,skip_header=1)
 
-f = T[:,0]
-Spec = T[:,1]
+f0020 = T0020[:,0]
+Spec0020 = T0020[:,1]
+
+f0550 = T0550[:,0]
+Spec0550 = T0550[:,1]
+
+f2000 = T2000[:,0]
+Spec2000 = T2000[:,1]
 
 # =============================================================================
 #  Conversion temporel --> spatial
@@ -32,42 +42,89 @@ Spec = T[:,1]
 # mean_U = [2.3158,  4.1619, 5.0156]
 # =============================================================================
 nu = 1.5e-5 #m/s à vérifier
-U1 = 2.3158 #m/s
-K1 = 2*np.pi*f/U1
-F11_K1 = Spec*U1/(2*np.pi)
+U0020 = 2.3158 #m/s
+U0550 = 4.1619
+U2000 = 5.0156
+
+
+K1_0020 = 2*np.pi*f0020/U0020
+F11_K1_0020 = Spec0020*U0020/(2*np.pi)
+
+K1_0550 = 2*np.pi*f0550/U0550
+F11_K1_0550 = Spec0550*U0550/(2*np.pi)
+
+K1_2000 = 2*np.pi*f2000/U2000
+F11_K1_2000 = Spec2000*U2000/(2*np.pi)
 
 # =============================================================================
 # Selections des wavenumber LP
 # =============================================================================
-K_cut = 7e3
-id_K_cut = np.argwhere(K1>K_cut)[0,0]
-K1 = K1[:id_K_cut]
-F11_K1 = F11_K1[:id_K_cut] 
+K_cut_0020 = 7e3
+id_K_cut_0020 = np.argwhere(K1_0020>K_cut_0020)[0,0]
+K1_0020 = K1_0020[:id_K_cut_0020]
+F11_K1_0020 = F11_K1_0020[:id_K_cut_0020] 
 
-dK = np.mean(np.diff(K1))
-K = K1[:-2]
-Klog = np.logspace(np.log10(np.min(K1)),np.log10(np.max(K1)),len(K))
+K_cut_0550 = 5e3
+id_K_cut_0550 = np.argwhere(K1_0550>K_cut_0550)[0,0]
+K1_0550 = K1_0550[:id_K_cut_0550]
+F11_K1_0550 = F11_K1_0550[:id_K_cut_0550] 
+
+K_cut_2000 = 3.5e3
+id_K_cut_2000 = np.argwhere(K1_2000>K_cut_2000)[0,0]
+K1_2000 = K1_2000[:id_K_cut_2000]
+F11_K1_2000 = F11_K1_2000[:id_K_cut_2000] 
+
+
+dK0020 = np.mean(np.diff(K1_0020))
+K0020 = K1_0020[:-2]
+
+dK0550 = np.mean(np.diff(K1_0550))
+K0550 = K1_0550[:-2]
+
+dK2000 = np.mean(np.diff(K1_2000))
+K2000 = K1_2000[:-2]
+
+#Klog = np.logspace(np.log10(np.min(K1)),np.log10(np.max(K1)),len(K))
 #tck = splrep(K1,F11_K1)
 #F11_Klog = splev(Klog,tck) 
 # =============================================================================
 # Filtrage de F11_K1
 # =============================================================================
 
-b,a = signal.butter(5,4.8e-3,fs=1./dK) # 0.03 marche pas mal
+b0020,a0020 = signal.butter(5,4.8e-3,fs=1./dK0020) # 0.03 marche pas mal
+F11_filt_0020 = signal.filtfilt(b0020,a0020,F11_K1_0020)
 #F11_filt = np.exp(signal.filtfilt(b,a,np.log(F11_Klog)))
-F11_filt = signal.filtfilt(b,a,F11_K1)
+
+b0550,a0550 = signal.butter(5,4.8e-3,fs=1./dK0550) # 0.03 marche pas mal
+F11_filt_0550 = signal.filtfilt(b0550,a0550,F11_K1_0550)
+
+b2000,a2000 = signal.butter(5,4.8e-3,fs=1./dK2000) # 0.03 marche pas mal
+F11_filt_2000 = signal.filtfilt(b2000,a2000,F11_K1_2000)
+
+
 
 # =============================================================================
 # Conversion F11_K1 --> E(K)
 # =============================================================================
-bE,aE = signal.butter(3,1.4e-3,fs=1./dK)
-E = np.power(K,3.)*np.diff( (1./K1[:-1])*np.diff(F11_filt)/dK )/dK
-E_filt = signal.filtfilt(bE,aE,E)
+bE0020,aE0020 = signal.butter(3,1.4e-3,fs=1./dK0020)
+E0020 = np.power(K0020,3.)*np.diff( (1./K1_0020[:-1])*np.diff(F11_filt_0020)/dK0020 )/dK0020
+E_filt0020 = signal.filtfilt(bE0020,aE0020,E0020)
+
+bE0550,aE0550 = signal.butter(3,1.4e-3,fs=1./dK0550)
+E0550 = np.power(K0550,3.)*np.diff( (1./K1_0550[:-1])*np.diff(F11_filt_0550)/dK0550 )/dK0550
+E_filt0550 = signal.filtfilt(bE0550,aE0550,E0550)
+
+bE2000,aE2000 = signal.butter(3,1.4e-3,fs=1./dK2000)
+E2000 = np.power(K2000,3.)*np.diff( (1./K1_2000[:-1])*np.diff(F11_filt_2000)/dK2000 )/dK2000
+E_filt2000 = signal.filtfilt(bE2000,aE2000,E2000)
+
+
+
 
 plt.figure();
-plt.plot(K1,F11_K1,label='Raw F_11');
-plt.plot(K1,F11_filt,label='Filt. F_11');
-plt.plot(K,E_filt,label='Filt. E')
+plt.plot(K1_0020,F11_K1_0020,label='Raw F_11');
+plt.plot(K1_0020,F11_filt_0020,label='Filt. F_11');
+plt.plot(K0020,E_filt0020,label='Filt. E')
 plt.legend()
 plt.xscale('log');plt.yscale('log')
 
@@ -84,36 +141,69 @@ plt.xscale('log');plt.yscale('log')
 # =============================================================================
 
 #epsilon = 2*nu*np.sum( K1*K1*F11_K1)*np.mean(np.diff(K1))
-epsilon = 2*nu*np.sum( (K**2)*E*dK)
+epsilon0020 = 2*nu*np.sum( (K0020**2)*E0020*dK0020)
+mean_U2_0020 = (1./3.)*np.sum(E0020)*dK0020  #--> A peu près ok
 
-mean_U2 = (1./3.)*np.sum(E)*dK  #--> A peu près ok
+epsilon0550 = 2*nu*np.sum( (K0550**2)*E0550*dK0550)
+mean_U2_0550 = (1./3.)*np.sum(E0550)*dK0550
+
+epsilon2000 = 2*nu*np.sum( (K2000**2)*E2000*dK2000)
+mean_U2_2000 = (1./3.)*np.sum(E2000)*dK2000
 
 # =============================================================================
 #  Non dimensionnalize
 # =============================================================================
 
-eta = np.power(nu**3/epsilon,0.25)
+eta0020 = np.power(nu**3/epsilon0020,0.25)
+F11star0020 = F11_K1_0020*np.power(nu,-5./4.)*np.power(epsilon0020,-1./4.)
+F0020 = E_filt0020*np.power(nu,-5./4.)*np.power(epsilon0020,-1./4.)
+Kstar0020 = K0020*eta0020
 
-F11star = F11_K1*np.power(nu,-5./4.)*np.power(epsilon,-1./4.)
-F = E_filt*np.power(nu,-5./4.)*np.power(epsilon,-1./4.)
+eta0550 = np.power(nu**3/epsilon0550,0.25)
+F11star0550 = F11_K1_0550*np.power(nu,-5./4.)*np.power(epsilon0550,-1./4.)
+F0550 = E_filt0550*np.power(nu,-5./4.)*np.power(epsilon0550,-1./4.)
+Kstar0550 = K0550*eta0550
 
-Kstar = K*eta
+eta2000 = np.power(nu**3/epsilon2000,0.25)
+F11star2000 = F11_K1_2000*np.power(nu,-5./4.)*np.power(epsilon2000,-1./4.)
+F2000 = E_filt2000*np.power(nu,-5./4.)*np.power(epsilon2000,-1./4.)
+Kstar2000 = K2000*eta2000
 
 #Kstarlog = np.logspace(np.log10(np.min(Kstar)),np.log10(np.max(Kstar)))
 #F_Kolmogorov = 0.5*np.power(epsilon,2./3.)*np.power(Kstarlog,-5./3.)*np.power(nu,-5./4.)*np.power(epsilon,-1./4.)
 
 plt.figure()
-plt.plot(K1*eta,F11star)
+plt.plot(K1_0020*eta0020,F11star0020, label='0.20')
+plt.plot(K1_0550*eta0550,F11star0550, label='5.50')
+plt.plot(K1_2000*eta2000,F11star2000, label='20.0')
 plt.xlabel('Kstar')
 plt.ylabel('F11 star')
 plt.xscale('log')
 plt.yscale('log')
+plt.legend(title='$y$ (mm)')
+plt.tight_layout()
+
+
+#plt.figure()
+#plt.plot(K1_0020*eta0020,F11_filt_0020*np.power(nu,-5./4.)*np.power(epsilon0020,-1./4.), label='0.20')
+#plt.plot(K1_0550*eta0550,F11_filt_0550*np.power(nu,-5./4.)*np.power(epsilon0550,-1./4.), label='5.50')
+#plt.plot(K1_2000*eta2000,F11_filt_2000*np.power(nu,-5./4.)*np.power(epsilon2000,-1./4.), label='20.00')
+#plt.xlabel('Kstar')
+#plt.ylabel('F11 star')
+#plt.xscale('log')
+#plt.yscale('log')
+#plt.legend(title='$y$ (mm)')
+#plt.tight_layout()
+
 
 
 plt.figure()
-#plt.plot(Kstarlog,Kstarlog**(-5/3))
-plt.plot(Kstar,F)
+plt.plot(Kstar0020,F0020,label='0.20')
+plt.plot(Kstar0550,F0550,label='5.50')
+plt.plot(Kstar2000,F2000,label='20.00')
 plt.xlabel('Kstar')
 plt.ylabel('Fstar')
 plt.xscale('log')
 plt.yscale('log')
+plt.legend(title='$y$ (mm)')
+plt.tight_layout()
--- Python/U_Momentum_Analysis.py
+++ Python/U_Momentum_Analysis.py
@@ -42,12 +42,20 @@ u_kurt = T[:,7]
 
 # Log fitting
 def cost_fit(x):
-    mean_u_fit = x[0]*np.log(y) + x[1]
-    return np.sqrt(np.mean(np.square(mean_u_fit-mean_U))/np.mean(np.square(mean_U)))
+    mean_u_fit = x[0]*np.log(y[2:-4]) + x[1]
+    return np.sqrt(np.mean(np.square(mean_u_fit-mean_U[2:-4]))/np.mean(np.square(mean_U[2:-4])))
 res = scipy.optimize.minimize(cost_fit,[1,1])
 x = res.x
 yfit = np.linspace(np.min(y),np.max(y),1000)
 mean_u_fit = x[0]*np.log(yfit) + x[1]
+
+
+def cost_lin(alpha):
+    mean_u_lin = alpha*y[:2]
+    return np.sqrt(np.mean(np.square((mean_u_lin - mean_U[:2])))/np.mean(np.square(mean_U[:2])))
+
+res_lin = scipy.optimize.minimize(cost_lin,[1.])
+alpha = res_lin.x[0]
 
 
 # Log-law fitting
@@ -56,19 +64,32 @@ a = 5.2
 ustar = x[0]*k
 nu = ustar*np.exp(-k*(x[1]/ustar-a))
 
+nu_theo = 1.5e-5
+y_5 = nu*ustar*5*1e3 # y (mm) @ which y+ = 5
+y_30 = nu_theo*ustar*30*1e3 # y (mm) @ wjich y+ = 30
+
+
+#ustar = np.sqrt(alpha*nu_theo)
+
 print('Log-law fitting with coefficients (%.3e,%.3e)'%(x[0],x[1]))
 print('Normalised residual RMS error : %.3e' % cost_fit(x))
 
+h = 30.
 
 plt.figure()
-plt.plot(yfit,mean_u_fit)
-plt.plot(y,mean_U,linestyle='dotted', marker='o',markersize=5.)
-plt.xlabel('Distance from wall $y$ - log scale (mm)')
+plt.plot(yfit/h,mean_u_fit)
+plt.plot(y/h,mean_U,linestyle='dotted', marker='o',markersize=5.)
+plt.xlabel('Distance from wall $eta = y/h$ - log scale')
 plt.ylabel('Mean velocity (m/s)')
 plt.xscale('log')
+#plt.yscale('log')
+plt.vlines(y_30/h,np.min(mean_U),np.max(mean_U),linestyle='dashed')
+plt.text(y_30/h*1.2, 4., '$y^{+} = 30$')
+plt.vlines(0.3,np.min(mean_U),np.max(mean_U),linestyle='dashed')
+plt.text(0.34, 3.5, '$\\eta = \\frac{y}{h} = 0.3$')
 plt.tight_layout()
-plt.savefig('Mean_Velocity_Profile.pgf')
-plt.close()
+#plt.savefig('Mean_Velocity_Profile.pgf')
+#plt.close()
 # A comparer avec log law cf. Pope p.274
 
 # =============================================================================
